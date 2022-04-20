6d405f619978df1f69a8cf8723f9c0a49289c382
Vitaly Cloud <vitalycloud@gmail.com>
2022-04-20 19:14:49 +0300

    Initial Commit

---
+++ ShaderEditor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ShaderEditor
+//
+//  Created by Vitaly Cloud on 20.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
