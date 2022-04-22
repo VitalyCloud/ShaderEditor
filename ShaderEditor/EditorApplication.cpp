c7257c21174cb352af0f52e7d2f51469678c3674
Vitaly Cloud <vitalycloud@gmail.com>
2022-04-23 00:09:32 +0300

    Clean up

--- ShaderEditor/EditorApplication.cpp
+++ ShaderEditor/EditorApplication.cpp
@@ -30,6 +30,72 @@ public:
     
     EditorLayer() : m_Camera(-1.0f, 1.0f, -1.0f, 1.0f) {}
     
+    virtual void OnEvent(Engine::Event& event) override {
+        m_TextEditor.OnEvent(event);
+    }
+    
+    void OnViewportResize(uint32_t width, uint32_t height) {
+        EN_INFO("Viewport resized: {0}x{1}", width, height);
+        
+        float A = (float)width / (float)height;
+        
+        float x = 1;
+        float y = 1;
+        x *= A;
+    
+        m_Camera.SetCamera(-x, x, -y, y);
+    }
+    
+    void OnMainMenuBar() {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Exit"))
+            {
+                Engine::Application::Get().Close();
+            }
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Window")) {
+            if (ImGui::MenuItem("Viewport", nullptr, m_ShowViewport))
+                m_ShowViewport = !m_ShowViewport;
+            if (ImGui::MenuItem("Text Editor", nullptr, m_ShowTextEditor))
+                m_ShowTextEditor = !m_ShowTextEditor;
+            if (ImGui::MenuItem("ImGui Demo", nullptr, m_ShowImGuiDemo))
+                m_ShowImGuiDemo = !m_ShowImGuiDemo;
+            if (ImGui::MenuItem("Debug", nullptr, m_ShowDebug))
+                m_ShowDebug = !m_ShowDebug;
+            ImGui::EndMenu();
+        }
+        
+        if (ImGui::BeginMenu("Settings")) {
+            bool VSync = Engine::Application::Get().IsVSync();
+            if (ImGui::MenuItem("VSync", nullptr, VSync))
+                Engine::Application::Get().SetVSync(!VSync);
+            
+            if(ImGui::BeginMenu("Style")) {
+                if (ImGui::MenuItem("Light"))
+                    ImGui::StyleColorsLight();
+                if (ImGui::MenuItem("Dark"))
+                    ImGui::StyleColorsDark();
+                if (ImGui::MenuItem("Classic"))
+                    ImGui::StyleColorsClassic();
+                
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::Separator();
+        {
+            const float FramesPerSecond = ImGui::GetIO().Framerate;
+            const float Frametime = 1000.0f / FramesPerSecond;
+            const auto fps = fmt::format("{0:.2f} FPS ({1:.2f} ms)",
+                FramesPerSecond, Frametime);
+            auto size = ImGui::CalcTextSize(fps.c_str());
+            ImGui::SameLine(0, ImGui::GetContentRegionAvail().x - size.x);
+            ImGui::Text("%s", fps.c_str());
+        }
+    }
+    
     virtual void OnAttach() override {
         OpenGL::FramebufferSpecification spec;
         spec.Width = 800;
@@ -39,53 +105,7 @@ public:
         m_Viewport.SetResizeCallback(std::bind(&EditorLayer::OnViewportResize,
                                                this, std::placeholders::_1, std::placeholders::_2));
         
-        Engine::Application::Get().SetMenubarCallback([this]() {
-            if (ImGui::BeginMenu("File"))
-            {
-                if (ImGui::MenuItem("Exit"))
-                {
-                    Engine::Application::Get().Close();
-                }
-                ImGui::EndMenu();
-            }
-            if (ImGui::BeginMenu("Windows")) {
-                if (ImGui::MenuItem("Viewport", nullptr, m_ShowViewport))
-                    m_ShowViewport = !m_ShowViewport;
-                if (ImGui::MenuItem("Text Editor", nullptr, m_ShowTextEditor))
-                    m_ShowTextEditor = !m_ShowTextEditor;
-                if (ImGui::MenuItem("ImGui Demo", nullptr, m_ShowImGuiDemo))
-                    m_ShowImGuiDemo = !m_ShowImGuiDemo;
-                ImGui::EndMenu();
-            }
-            
-            if (ImGui::BeginMenu("Settings")) {
-                bool VSync = Engine::Application::Get().IsVSync();
-                if (ImGui::MenuItem("VSync", nullptr, VSync))
-                    Engine::Application::Get().SetVSync(!VSync);
-                
-                if(ImGui::BeginMenu("Style")) {
-                    if (ImGui::MenuItem("Light"))
-                        ImGui::StyleColorsLight();
-                    if (ImGui::MenuItem("Dark"))
-                        ImGui::StyleColorsDark();
-                    if (ImGui::MenuItem("Classic"))
-                        ImGui::StyleColorsClassic();
-                    
-                    ImGui::EndMenu();
-                }
-                ImGui::EndMenu();
-            }
-            ImGui::Separator();
-            {
-                const float FramesPerSecond = ImGui::GetIO().Framerate;
-                const float Frametime = 1000.0f / FramesPerSecond;
-                const auto fps = fmt::format("{0:.2f} FPS ({1:.2f} ms)",
-                    FramesPerSecond, Frametime);
-                auto size = ImGui::CalcTextSize(fps.c_str());
-                ImGui::SameLine(0, ImGui::GetContentRegionAvail().x - size.x);
-                ImGui::Text("%s", fps.c_str());
-            }
-        });
+        Engine::Application::Get().SetMenubarCallback(std::bind(&EditorLayer::OnMainMenuBar, this));
         
         float a = 0.5f;
         
@@ -153,6 +173,8 @@ public:
         m_Shader = Engine::CreateRef<OpenGL::Shader>(vertexSrc, fragmentSrc);
     }
     
+    
+    
     virtual void OnUpdate() override {
         m_Camera.SetPostion(m_CameraPostion);
         
@@ -170,14 +192,16 @@ public:
     }
     
     virtual void OnUIRender() override {
-        ImGui::Begin("Debug");
-        ImGui::Text("Clear color:");
-        ImGui::SameLine();
-        ImGui::ColorEdit4("##ClearColorInput", &m_ClearColor.x);
-        ImGui::Text("Camera position:");
-        ImGui::SameLine();
-        ImGui::DragFloat3("##CameraPositionInput", &m_CameraPostion.x, 0.1, -10, 10);
-        ImGui::End();
+        if(m_ShowDebug) {
+            ImGui::Begin("Debug", &m_ShowDebug);
+            ImGui::Text("Clear color:");
+            ImGui::SameLine();
+            ImGui::ColorEdit4("##ClearColorInput", &m_ClearColor.x);
+            ImGui::Text("Camera position:");
+            ImGui::SameLine();
+            ImGui::DragFloat3("##CameraPositionInput", &m_CameraPostion.x, 0.1, -10, 10);
+            ImGui::End();
+        }
         
         if(m_ShowViewport)
             m_Viewport.Draw("Viewport", &m_ShowViewport);
@@ -189,23 +213,6 @@ public:
             ImGui::ShowDemoWindow(&m_ShowImGuiDemo);
     }
     
-    virtual void OnEvent(Engine::Event& event) override {
-        m_TextEditor.OnEvent(event);
-    }
-    
-    void OnViewportResize(uint32_t width, uint32_t height) {
-        EN_INFO("Viewport resized: {0}x{1}", width, height);
-        
-        float A = (float)width / (float)height;
-        
-        float x = 1;
-        float y = 1;
-        x *= A;
-    
-        m_Camera.SetCamera(-x, x, -y, y);
-        
-    }
-    
 private:
     ViewportPanel m_Viewport;
     TextEditorPanel m_TextEditor;
@@ -213,6 +220,7 @@ private:
     bool m_ShowViewport = true;
     bool m_ShowTextEditor = true;
     bool m_ShowImGuiDemo = false;
+    bool m_ShowDebug = true;
     
     Engine::Ref<OpenGL::Framebuffer> m_Framebuffer;
     Engine::Ref<OpenGL::VertexArray> m_VertexArray;
